90679baaf0e703808bb4245f20777af334d55843
lohithveerepalli <lohith12@uab.edu>
2025-03-27 19:30:39 -0500

    Update portfolio with modern Next.js design

---
+++ _next/static/chunks/app/layout-4f431a79f7c3280d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},8161:(e,s,n)=>{Promise.resolve().then(n.bind(n,6671)),Promise.resolve().then(n.t.bind(n,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[690,671,441,684,358],()=>s(8161)),_N_E=e.O()}]);
